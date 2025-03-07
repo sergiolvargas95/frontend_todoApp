175f54ebf0bc0320c57679981a2adaaae85f5659
sergiolvargas95 <sergiolvargas95@gmail.com>
2025-03-07 05:09:11 -0500

    add: filter tasks

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { Form, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { User } from '../../models/user.model';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
+
+@Component({
+  standalone: true,
+  selector: 'app-login',
+  imports: [ ReactiveFormsModule, RouterModule ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+  user: User;
+  userToken?: string;
+
+  constructor(
+    private fb:FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {
+    this.createLoginForm();
+    this.user = new User();
+  }
+
+  createLoginForm() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  login() {
+    if(this.loginForm.invalid) { return; }
+
+    this.user.email = this.loginForm.get('email')?.value;
+    this.user.password = this.loginForm.get('password')?.value;
+
+    this.auth.login(this.user).subscribe( resp => {
+      this.router.navigateByUrl('/home');
+    }, (err) => {
+      Swal.fire({
+        icon: 'error',
+        title: 'error while authenticating',
+        text: err.error.error.message,
+      })
+    });
+  }
+
+  get invalidEmail() {
+    return this.loginForm.get('email')?.invalid && this.loginForm.get('email')?.touched;
+  }
+
+  get invalidPassword() {
+    return this.loginForm.get('password')?.invalid && this.loginForm.get('password')?.touched;
+  }
+
+  readToken() {
+    if(localStorage.getItem('token')) {
+      this.userToken = JSON.parse(localStorage.getItem('token') as string);
+    } else {
+      this.userToken = '';
+    }
+    return this.userToken;
+  }
+}
