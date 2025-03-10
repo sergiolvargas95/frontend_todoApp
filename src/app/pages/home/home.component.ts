f86a69ade7dab805cad0561025d13db780ca2815
sergiolvargas95 <sergiolvargas95@gmail.com>
2025-03-10 11:45:04 -0500

    delete: angular material

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, HostListener, signal } from '@angular/core';
 import { TasksListComponent } from "../../components/tasks/tasks.component";
 import { TasksFilterComponent } from "../../components/tasks-filter/tasks-filter.component";
-import { MatDialog } from '@angular/material/dialog';
-import { TaskModalComponent } from '../../components/task-modal/task-modal.component';
 
 @Component({
   selector: 'app-home',
@@ -12,19 +10,4 @@ import { TaskModalComponent } from '../../components/task-modal/task-modal.compo
 })
 export class HomeComponent {
   $title = 'My Tasks';
-
-  constructor(private dialog: MatDialog) {}
-
-  openTaskModal() {
-    const dialogRef = this.dialog.open(TaskModalComponent, {
-      width: '400px'
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        console.log('New Task:', result);
-        // Aquí puedes enviar la tarea al backend o agregarla a la lista de tareas
-      }
-    });
-  }
 }
